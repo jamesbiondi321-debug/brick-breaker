65dd5cc8c93bef3c5657f2a5dbdf9ff7ba822d2a
jamesbiondi321-debug <james.biondi321@gmail.com>
2026-02-13 10:14:01 +1100

    raylib properly set up

--- brick-breaker/main.cpp
+++ brick-breaker/main.cpp
@@ -1,9 +1,21 @@
 #include <cstdlib>
 #include <iostream>
 
+#include<raylib.h>
+
 int main(int argC, char* argV[])
 {
-	std::cout << "Hello, World!\n";
+	InitWindow(1080, 720, "Brick Breaker");
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+
+		DrawCircle(540, 360, 50.f, RED);
+
+		EndDrawing();
+	}
 
 	return EXIT_SUCCESS;
 }
